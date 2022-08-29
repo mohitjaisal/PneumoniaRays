89b89354bec47c7ad43f6543a3a782996706910c
Mohit Jaisal <mohitjaisal19@gmail.com>
2022-08-29 22:23:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
